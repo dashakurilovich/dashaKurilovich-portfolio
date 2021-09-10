5cbe520ab086c18806f1ce8e70684d75eb781213
dashakurilovich <Dasha23_@mail.ru>
2021-09-10 21:47:04 +0300

    adapted

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -8,16 +8,12 @@ function Contacts() {
         <div className={styles.wrapContacts}>
             <div className={`${styleContainer.conteiner} ${styles.myContactsConteiner}`}>
                 <h2 className={styles.title}>Контакты</h2>
-                <div className={styles.shape}>
-                    <form className={styles.form}>
-                        <input className={styles.input} />
-                        <input className={styles.input} />
-                        <textarea />
-                    </form>
-                </div>
-                <div>
-                    <button>Отправить</button>
-                </div>
+                <form className={styles.form}>
+                    <input className={styles.input} />
+                    <input className={styles.input} />
+                    <textarea className={styles.textarea} />
+                    <button className={styles.button}> Отправить </button>
+                </form>
             </div>
         </div>
     );
